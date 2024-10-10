7bd0fae3bc36d21c850d41a74b844e63e6afff88
Rudwolf12 <rodolfopemc@outlook.com>
2024-10-10 01:12:21 -0600

    I don't know

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+console.log("¡Hola mundo desde JavaScript!");
+
+function UnfoldMobileMenu()
+{
+    if (document.querySelector('.menu_drop').style.display == 'block')
+    {
+        document.querySelector('.menu_drop').style.display = 'none';
+    }
+    else
+    {
+        document.querySelector('.menu_drop').style.display = 'block';
+    }
+}
